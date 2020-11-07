050d925fe67a8415266a18c844a88f20e44e6ca3
Ayodeji <46384451+dej10@users.noreply.github.com>
2020-11-07 15:42:54 +0100

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -23,11 +24,11 @@ const userSchema = new mongoose.Schema({
 	password: String
 });
 
+const secret = "Thetreeisgreeniatthenorthsideentranceofmyhouseinportugal@";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+
 
 const User = mongoose.model("User", userSchema);
-
-
-
 
 
 app.get("/", function(req, res) {
